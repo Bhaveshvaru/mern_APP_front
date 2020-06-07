636c8feecebdd7d2ee34f01eafbb5ba126f1d58b
firstname lastname <your email@emailprovider.com>
2020-06-08 01:15:18 +0530

    added stripe  payment component

--- src/core/helper/StripeCheckout.js
+++ src/core/helper/StripeCheckout.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { isAuthenticated } from '../../auth/helper';
 import { cartEmpty, loadCart } from './cartHelper';
 import { Link } from 'react-router-dom';
+import StripeCheckOut from 'react-stripe-checkout';
+import { API } from '../../backend';
+import { createOrder } from './orderHelper';
 
 const StripeCheckout = ({
   products,
@@ -25,10 +28,40 @@ const StripeCheckout = ({
     });
     return amount;
   };
+  const makePayment = (token) => {
+    const body = {
+      token,
+      products,
+    };
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+    fetch(`${API}/stripePayment`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        console.log(response);
+        const { status } = require(response);
+        console.log('STATUS', status);
+       // cartEmpty();
+      })
+      .catch((err) => console.log(err));
+  };
 
   const showStripeButton = () => {
     return isAuthenticated() ? (
-      <button className="btn btn-success">Pay with Stripe</button>
+      <StripeCheckOut
+        stripeKey="pk_test_51GrMwNHWj5wEAALnR0FqtCdlPGHgQD9H62lnhpIOXQRhOVIuNuRp5qyM2L81IAnACyN3YbzNpf6uMReMTefOpZFx00FNrY2Xts"
+        token={makePayment}
+        amount={getFinalAmount() * 100}
+        name="Buy Item"
+        shippingAddress
+        billingAddress
+      >
+        <button className="btn btn-success">Pay with Stripe</button>
+      </StripeCheckOut>
     ) : (
       <Link to="/signin">
         <button className="btn btn-warning">Sign In</button>
